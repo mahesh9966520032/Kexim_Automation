b31cdc1f08c0a9cb30f48c5083321fb36a8464cb
mindq <mindq@mindq-PC>
2015-12-24 08:25:27 +0530

    PageObjects adding

---
+++ kexim_Automation_17122015/src/com/kexim/PageObjects/PageObjects.java
@@ -0,0 +1,44 @@
+package com.kexim.PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import com.kexim.Locators.Locators;
+
+public class PageObjects {
+
+	//Kexim Home page Locators
+	public static class KexmimHome
+	{
+		//lst Branch Name PageObject
+		public static Select lstBranchName(WebDriver driver)
+		{
+	        return new Select(driver.findElement(Locators.KexmimHome.lstBranchName));		
+		}
+	
+		//txt UserName PageObject
+		public static Select txtUserName(WebDriver driver)
+		{
+	        return new Select(driver.findElement(Locators.KexmimHome.txtUserName));		
+		}
+
+		
+	}//keximHome end
+	
+	
+	//Global PageObjects
+	public static class Global
+	{
+
+		
+					
+	}//Global end
+	
+	//BranchesHome PageObjects
+	public static class BranchesHome
+	{
+		//btn NewBranch
+		
+	}//BranchesHome end
+	
+}

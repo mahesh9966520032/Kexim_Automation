040b32890d23026bd5c7867c5fcae76c73c4ae9d
mindq <mindq@mindq-PC>
2016-01-29 10:13:12 +0530

    total project

--- Kexim_Automation_12162015/src/com/kexim/sanitychecks/TestMaker.java
+++ Kexim_Automation_12162015/src/com/kexim/sanitychecks/TestMaker.java
@@ -2,7 +2,6 @@ package com.kexim.sanitychecks;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.testng.TestNG;
 import org.testng.annotations.Test;
 
@@ -12,13 +11,16 @@ public class TestMaker {
 	  
 	  //for integrating suite files
 	  TestNG testng=new TestNG();
+	  
 	  List<String> suites=new ArrayList();
 	  //add suite files to arraylist
 	  suites.add("admintestsuite.xml");
 	  suites.add("branchestestsuite.xml");
 	  //pass arraylist object to testng settestsuites method
 	  testng.setTestSuites(suites);
-	  
+	  System.out.println("************************");
+	  System.out.println("TestNG Running");
+	  System.out.println("************************");
 	  testng.run();
 	  
 	  
